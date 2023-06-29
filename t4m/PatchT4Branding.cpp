78ab988b25d9567960404921eae51ed35c0578df
Alex <45800460+JBShady@users.noreply.github.com>
2023-06-28 23:50:18 -0700

    Update PatchT4Branding.cpp
    T4Me re-brand/r48

--- t4m/PatchT4Branding.cpp
+++ t4m/PatchT4Branding.cpp
@@ -19,7 +19,7 @@ void PatchT4_Branding()
 	// TODO: Replace shortversion DVars and other version related locations
 	//nop(0x59D68B, 5);										// commented out to re-enable intro video because it's cool
 	nop(0x5FD91B, 5);										// disable pc_newversionavailable check
-	PatchMemory(0x851208, (PBYTE)"T4Me r47> ", 14);	// change the console input version
+	PatchMemory(0x851208, (PBYTE)"T4Me r48> ", 14);	// change the console input version
 	PatchMemory(0x871EE8, (PBYTE)va("T4-SP (r%i)\n", VERSION), 32);
 	Detours::X86::DetourFunction((PBYTE)0x5B5A20, (PBYTE)&SetShortVersion, Detours::X86Option::USE_CALL); // change version number bottom right of main
 	Detours::X86::DetourFunction((PBYTE)0x4743D2, (PBYTE)&SetConsoleVersion, Detours::X86Option::USE_CALL); // change the version info of console window
