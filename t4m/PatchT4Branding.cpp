2a0e9df1f4380a6572107dd03a96e9e288e94128
Clippy95 <qpzezoqp15@gmail.com>
2025-11-22 00:14:49 +0300

    add version_t4me dvar

--- t4m/PatchT4Branding.cpp
+++ t4m/PatchT4Branding.cpp
@@ -16,10 +16,11 @@ const char* SetShortVersion();
 
 void PatchT4_Branding()
 {
+	Dvar_RegisterInt(VERSION_T4ME, "version_t4me", 0, VERSION_T4ME, DVAR_FLAG_ROM);
 	// TODO: Replace shortversion DVars and other version related locations
 	//nop(0x59D68B, 5);										// commented out to re-enable intro video because it's cool
 	nop(0x5FD91B, 5);										// disable pc_newversionavailable check
-	PatchMemory(0x851208, (PBYTE)"T4Me r49> ", 14);	// change the console input version
+	PatchMemory(0x851208, (PBYTE)va("T4Me r%d> ", VERSION_T4ME), 14);	// change the console input version
 	PatchMemory(0x871EE8, (PBYTE)va("T4-SP (r%i)\n", VERSION), 32);
 	Detours::X86::DetourFunction((PBYTE)0x5B5A20, (PBYTE)&SetShortVersion, Detours::X86Option::USE_CALL); // change version number bottom right of main
 	Detours::X86::DetourFunction((PBYTE)0x4743D2, (PBYTE)&SetConsoleVersion, Detours::X86Option::USE_CALL); // change the version info of console window
@@ -28,11 +29,11 @@ void PatchT4_Branding()
 
 const char* SetConsoleVersion()
 {
-	return va("Call of Duty %s", "T4-SP (r49) (built " DATE " " TIME " by Clippy95 and Nazi Zombies remastered developer JB)");
+	return va("Call of Duty %s", va("T4-SP (r%d) (built " DATE " " TIME " by Clippy95 and Nazi Zombies remastered developer JB)", VERSION_T4ME));
 }
 
 const char* SetShortVersion()
 {
-	return va("2.0.49");
+	return va("2.0.%d", VERSION_T4ME);
 }
 
