14cba7e272ad737e1d7b15eda7446448b736012f
alexandreseve52 <alexsevesauvage@gmail.com>
2026-03-13 09:49:26 +0100

    Updated from upstream/master
    commit 0db617413646513facd68b6ccbeac3304ad132e1
    Author: Clippy95 <qpzezoqp15@gmail.com>
    Date:   Fri Jan 16 22:51:39 2026 +0300
    
        CG_DrawOverheadNames_scale_fix, the size of it got smaller at higher resolutions
    
    commit 8b4591ad9fcc03f849e3f85b37f6fa132604e29e
    Author: Clippy95 <qpzezoqp15@gmail.com>
    Date:   Tue Jan 6 16:56:26 2026 +0300
    
        cg_drawAmmoClipOffset,cg_drawAmmoReserveOffset & cg_drawAmmoMod archive
    
    commit 7733a6646518c5cd78129cfb6e569e467ab601b1
    Author: Clippy95 <qpzezoqp15@gmail.com>
    Date:   Sat Jan 3 16:54:52 2026 +0300
    
        Fix patching dynamicVertexBuffer as r_buf_skinnedCacheVb

--- t4m/PatchT4E_Render.cpp
+++ t4m/PatchT4E_Render.cpp
@@ -32,8 +32,6 @@ void* __cdecl R_CreateDynamicBuffers_hook() {
 		if (r_buf_skinnedCacheVb) {
 			// default 60 MB?
 			Patch<uint32_t>((0x0070EC77 + 1), r_buf_skinnedCacheVb->current.integer * MB_SIZE);
-			Patch<uint32_t>((0x0070EC42), r_buf_skinnedCacheVb->current.integer * MB_SIZE);
-			Patch<uint32_t>((0x70ED2C + 1), r_buf_skinnedCacheVb->current.integer * MB_SIZE);
 
 			Patch<uint32_t>((0x71DD7C + 2), ((uint32_t)((float)(r_buf_skinnedCacheVb->current.integer * MB_SIZE) / 1.44444444f))); // ok this idk f why
 			Patch<uint32_t>((0x71DD95 + 2), r_buf_skinnedCacheVb->current.integer * MB_SIZE);
@@ -52,11 +50,11 @@ void* __cdecl R_CreateDynamicBuffers_hook() {
 			Patch<uint32_t>((0x70EE1E + 1), r_buf_dynamicIndexBuffer->current.integer * MB_SIZE);
 		}
 
-		if (r_buf_preTessIndexBuffer) {
-			// default 2 MB
-			Patch<uint32_t>((0x0070EDEA + 3), (r_buf_preTessIndexBuffer->current.integer * MB_SIZE) / 2); // idk why
-			Patch<uint32_t>((0x70EE7D + 1), r_buf_preTessIndexBuffer->current.integer * MB_SIZE);
-		}
+		//if (r_buf_preTessIndexBuffer) {
+		//	// default 2 MB
+		//	Patch<uint32_t>((0x0070EDEA + 3), (r_buf_preTessIndexBuffer->current.integer * MB_SIZE) / 2); // idk why
+		//	Patch<uint32_t>((0x70EE7D + 1), r_buf_preTessIndexBuffer->current.integer * MB_SIZE);
+		//}
 	}
 	return R_CreateDynamicBuffers();
 }
@@ -277,13 +275,13 @@ void PatchT4E_Render() {
 		0,
 		"z position FOV offset compensation of the viewmodel");
 
-	r_buf_skinnedCacheVb = Dvar_RegisterInt(120, "r_buf_skinnedCacheVb", 60, 512, DVAR_FLAG_ARCHIVE);
-	r_buf_tempSkin = Dvar_RegisterInt(120, "r_buf_tempSkin", 60, 512, DVAR_FLAG_ARCHIVE);
-	r_buf_dynamicVertexBuffer = Dvar_RegisterInt(3, "r_buf_dynamicVertexBuffer", 1, 16, DVAR_FLAG_ARCHIVE);
-	r_buf_dynamicIndexBuffer = Dvar_RegisterInt(4, "r_buf_dynamicIndexBuffer", 2, 16, DVAR_FLAG_ARCHIVE);
-	r_buf_preTessIndexBuffer = Dvar_RegisterInt(4, "r_buf_preTessIndexBuffer", 2, 16, DVAR_FLAG_ARCHIVE);
+	r_buf_skinnedCacheVb = Dvar_RegisterInt(120, "r_buf_skinnedCacheVb", 60, 512, DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH);
+	r_buf_tempSkin = Dvar_RegisterInt(120, "r_buf_tempSkin", 60, 512, DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH );
+	r_buf_dynamicVertexBuffer = Dvar_RegisterInt(3, "r_buf_dynamicVertexBuffer", 1, 16, DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH);
+	r_buf_dynamicIndexBuffer = Dvar_RegisterInt(4, "r_buf_dynamicIndexBuffer", 2, 16, DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH );
+	r_buf_preTessIndexBuffer = Dvar_RegisterInt(4, "r_buf_preTessIndexBuffer", 2, 16, DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH );
 
-	r_increase_render_buffers = Dvar_RegisterBool(true, "r_increase_render_buffers", DVAR_FLAG_ARCHIVE, "increasing rendering buffers");
+	r_increase_render_buffers = Dvar_RegisterBool(true, "r_increase_render_buffers", DVAR_FLAG_ARCHIVE | DVAR_FLAG_LATCH, "increasing rendering buffers");
 
 	InterceptCall(0x6D594D, R_CreateDynamicBuffers, R_CreateDynamicBuffers_hook);
 
